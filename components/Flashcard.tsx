665d5eb4ed34d08e4dd9a01225ea862de91acc14
gormlabenz <gorm@labenz.io>
2023-12-22 13:56:48 +0100

    Refactor Flashcard component to use animated values for top and backgroundColor

--- components/Flashcard.tsx
+++ components/Flashcard.tsx
@@ -1,14 +1,58 @@
-import { FC } from 'react'
-import { Text, View } from 'react-native'
-import { colors } from '../lib/const'
+import { FC, useEffect, useRef, useState } from 'react'
+import { Animated, Text, View } from 'react-native'
 
 interface Props {
   children: React.ReactNode
-  backgroundColor?: string
+  backgroundColor: string
   top: number
 }
 
 const Flashcard: FC<Props> = ({ children, backgroundColor, top }: Props) => {
+  // Animated Values
+  const topAnimatedValue = useRef(new Animated.Value(top)).current
+  const backgroundColorAnimatedValue = useRef(new Animated.Value(0)).current
+
+  // Refs to track the previous values
+  const prevTopRef = useRef(top)
+  const prevBackgroundColorRef = useRef(backgroundColor)
+
+  // Interpolations
+  const topInterpolate = topAnimatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [prevTopRef.current, top],
+  })
+
+  const backgroundColorInterpolate = backgroundColorAnimatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [prevBackgroundColorRef.current, backgroundColor],
+  })
+
+  useEffect(() => {
+    // Update the previous values
+    prevTopRef.current = top
+    prevBackgroundColorRef.current = backgroundColor
+
+    // Animate top
+    Animated.timing(topAnimatedValue, {
+      toValue: 1, // We are interpolating to 1, so we animate to 1
+      duration: 300,
+      useNativeDriver: false,
+    }).start(() => {
+      // Reset the animated value to 0 after the animation is done
+      topAnimatedValue.setValue(0)
+    })
+
+    // Animate backgroundColor
+    Animated.timing(backgroundColorAnimatedValue, {
+      toValue: 1, // We are interpolating to 1, so we animate to 1
+      duration: 300,
+      useNativeDriver: false,
+    }).start(() => {
+      // Reset the animated value to 0 after the animation is done
+      backgroundColorAnimatedValue.setValue(0)
+    })
+  }, [top, backgroundColor, topAnimatedValue, backgroundColorAnimatedValue])
+
   return (
     <View
       style={{
@@ -19,9 +63,9 @@ const Flashcard: FC<Props> = ({ children, backgroundColor, top }: Props) => {
         width: '100%',
       }}
     >
-      <View
+      <Animated.View
         style={{
-          backgroundColor: backgroundColor ? backgroundColor : colors.gray[400],
+          backgroundColor: backgroundColorInterpolate,
           flex: 1,
           borderRadius: 16,
           margin: 1,
@@ -30,11 +74,11 @@ const Flashcard: FC<Props> = ({ children, backgroundColor, top }: Props) => {
           paddingBottom: 12,
           height: '100%',
           paddingVertical: 24,
-          top,
+          top: topInterpolate,
         }}
       >
         {children}
-      </View>
+      </Animated.View>
     </View>
   )
 }
